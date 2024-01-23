b26924185d9173060225681fee6d00ddfb2bab7c
chreamy <chrislinkou@gmail.com>
2024-01-23 08:39:29 -0600

    test data

---
+++ backend/routes/contest.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+router.route("/list").get(async (req, res) => {
+  //get the contests by posts
+});
+router.route("/:id").get(async (req, res) => {
+  // get the contests by ID
+});
---
+++ backend/schemas/contest.js
@@ -0,0 +1,35 @@
+//example with Mongo DB
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const contestSchema = new Schema({
+  name: { type: String, required: true },
+  date: { type: Date, required: true },
+  description: { type: String, required: true },
+  address: { type: String, required: true },
+  coordinates: {
+    type: [Number],
+    validate: {
+      validator: function (coordinates) {
+        return coordinates.length === 2;
+      },
+      message:
+        "Coordinates must be an array of two numbers [longitude, latitude]",
+    },
+  },
+  media: [String], //array of media links
+  tags: [String], //major, school, and other tags to sort contests
+  created: { type: Date, default: Date.now, required: true },
+  updated: { type: Date, default: Date.now, required: true },
+  status: {
+    type: String,
+    required: true,
+    enum: ["pending", "approved", "rejected", "archived"],
+    default: "pending",
+  },
+});
+
+const Contest = mongoose.model("Contests", contestSchema);
+
+module.exports = Contest;
