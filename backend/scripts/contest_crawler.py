07ee88e773610021a3f0b3e2c548c09c1ed82ad1
chris <116124688+chreamy@users.noreply.github.com>
2024-02-21 16:29:45 -0600

    feat: web crawler with detailed info

--- backend/scripts/contest_crawler.py
+++ backend/scripts/contest_crawler.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from fake_useragent import UserAgent
+from selenium.common.exceptions import NoSuchElementException
 import pandas as pd
+import json
+import threading
 import time
-import random
+from fake_useragent import UserAgent
+
+# prevent fields that don't exist from causing error
+def safe_find_element_text(parent, by, value):
+    try:
+        return parent.find_element(by, value).text
+    except NoSuchElementException:
+        return None
 
 # 爬取比賽的標題和連結
 def crawlContestTitleLink(driver):
@@ -13,7 +21,7 @@ def crawlContestTitleLink(driver):
     # 儲存比賽數量
     contestSum = 0
     # 迭代contest的數量
-    count = 1 
+    count = 1
 
     # 點擊更多按鈕，直到沒有更多按鈕為止
     while True:
@@ -49,45 +57,89 @@ def crawlContestTitleLink(driver):
         contestDataList.append(titleLinkDict)
         # df = pd.DataFrame(contestDataList)
 
-        time.sleep(2)
+        #time.sleep(2)
         count += 1
 
     saveContestData(contestDataList)
 
-    # 呼叫searchContest函式 
-    # searchContest(driver,contestDataList,contestSum)
+    # 呼叫searchContest函式
+    searchContest(driver,contestDataList,contestSum)
 
 # 爬取每個比賽的詳細資訊
 def searchContest(driver,contestDataList,contestSum):
     # 計算在contestDataList第幾個比賽
     index = 0
     while index < contestSum:
-        # 模擬搜尋比賽
-        search = driver.find_element(By.CLASS_NAME, "bh-search-input.bh-text-ui")
-        # 輸入關鍵字
-        search.send_keys(contestDataList[index]['title'])
-        # 模擬點擊搜尋按鈕
-        search.send_keys(Keys.RETURN)
-        time.sleep(2)
-
-        # 取得比賽的link
-        contestLink = driver.find_element(By.CLASS_NAME, "bh-title-block")
+    
+        contestLink = contestDataList[index]['link']
         # 點擊比賽的link
         # 使用ActionChains進行點擊
-        ActionChains(driver).move_to_element(contestLink).click().perform()
+        driver.get(contestLink)
         time.sleep(2)
         
-        dateInfo = driver.find_element(By.CSS_SELECTOR, "div.bh-block.bh-range-block").text
+        # scrape dates
+        try:
+            parentElement = driver.find_element(By.CSS_SELECTOR, "div.bh-block.bh-range-block")
+            dateElements = parentElement.find_elements(By.CLASS_NAME, "bh-value")
+            if len(dateElements) > 2:
+                contestDataList[index]['startDate'] = dateElements[0].text
+                contestDataList[index]['endDate'] = dateElements[2].text
+        except NoSuchElementException:
+            pass
 
-        contestDetail = driver.find_element(By.CLASS_NAME, "bh-block.bh-content-block").text
+        # scrape organizer
+        organizer_elements = [("bh-item-organizer-title", "organizer"),("bh-item-agency-title", "agency"),("bh-item-contact-phone", "contactPhone"),("bh-item-contact-email", "contactEmail"),("bh-item-location", "location"),]
 
-
-        contestDataList[index]['date'] = dateInfo
-        contestDataList[index]['detail'] = contestDetail
-
+        try:
+            parentElement = driver.find_element(By.CSS_SELECTOR, "div.bh-block.bh-organizer-block")
+            for class_name, key in organizer_elements:
+                text = safe_find_element_text(parentElement, By.CLASS_NAME, class_name)
+                if text is not None and key!='organizer':
+                    contestDataList[index][key] = text[text.find('：')+1:]
+                elif text is not None:
+                    contestDataList[index][key] = text
+                
+        except NoSuchElementException:
+            pass
+                
+        #scrape tags
+        try:
+            parentElement = driver.find_element(By.CSS_SELECTOR, "div.bh-block.bh-category-tag-block")
+            elements = parentElement.find_elements(By.CLASS_NAME, "bh-value")
+            contestDataList[index]['tags'] = [i.text for i in elements]
+        except NoSuchElementException:
+            pass
+        
+        try:
+            contestDetail = driver.find_element(By.CLASS_NAME, "bh-block.bh-content-block").text
+            contestDataList[index]['detail'] = contestDetail
+        except NoSuchElementException:
+            pass
+        
+        #scrape info section
+        try:
+            parentElement = driver.find_element(By.CLASS_NAME, "bh-info-block")
+            elements = parentElement.find_elements(By.CLASS_NAME, "bh-item")
+            for element in elements:
+                key = element.find_element(By.CLASS_NAME, "bh-text")
+                value = element.find_element(By.CLASS_NAME, "bh-value")
+                if key.text in ['國籍/地區限制','總獎金','最高獎金','身分限制']:
+                    contestDataList[index]['nationality' if key.text == '國籍/地區限制' else 'totalPrize' if key.text == '總獎金' else 'maxPrize' if key.text == '最高獎金' else 'ageGroup'] = value.text or ''
+        except NoSuchElementException:
+            pass
+        
+        #scrape descriptions
+        try:
+            contestDetail = driver.find_element(By.CLASS_NAME, "bh-block.bh-content-block").text
+            contestDataList[index]['detail'] = contestDetail
+        except NoSuchElementException:
+            pass
+        
+        print('Scraped',contestDataList[index]['title'])
         index += 1
+        
     
-    # saveContestData(contestDataList)
+    saveContestData(contestDataList)
 
 # 將contestDataList存成csv和json檔
 def saveContestData(contestDataList):
@@ -95,7 +147,9 @@ def saveContestData(contestDataList):
     # 將contestDataList存成csv和json檔
     df = pd.DataFrame(contestDataList)
     df.to_csv('contest_data.csv', index=False)
-    df.to_json('contest_data.json', orient='records')
+    data = df.to_dict(orient='records')
+    with open('contest_data.json', 'w', encoding='utf-8') as file:
+        json.dump(data, file, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
@@ -117,7 +171,7 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)  
     driver.maximize_window()
-    driver.get('https://bhuntr.com/competitions')
+    driver.get('https://bhuntr.com/tw/competitions')
 
     crawlContestTitleLink(driver)
  
