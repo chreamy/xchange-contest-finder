12912995deafcd5f0ccbfaf4a38ea73654ba39e1
chreamy <chrislinkou@gmail.com>
2024-03-06 01:42:58 -0600

    feat: connect front and back, contest update and list API

--- frontend/src/pages/contestList.jsx
+++ frontend/src/pages/contestList.jsx
@@ -7,59 +7,6 @@ import axios from "axios";
 import { GoogleLogin } from "@react-oauth/google";
 import styles from "./contestList.module.css";
 
-<<<<<<< Updated upstream
-// class ContestList extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-//   componentDidMount() {}
-//   render() {
-//     return <h1>Hello World</h1>;
-//   }
-// }
-
-const ContestList = () => {
-  const [contests, setContests] = useState([
-    {
-      name: "比賽名稱",
-      date: "2024-01-31",
-      address: "???",
-      media: [""],
-    },
-    {
-      name: "比賽名稱",
-      date: "2024-01-31",
-      address: "???",
-      media: [""],
-    },
-    {
-      name: "比賽名稱",
-      date: "2024-01-31",
-      address: "???",
-      media: [""],
-    },
-    {
-      name: "比賽名稱",
-      date: "2024-01-31",
-      address: "???",
-      media: [""],
-    },
-    {
-      name: "比賽名稱",
-      date: "2024-01-31",
-      address: "???",
-      media: [""],
-    },
-    {
-      name: "比賽名稱",
-      date: "2024-01-31",
-      address: "???",
-      media: [""],
-    },
-  ]);
-  const handleGoogleLoginSuccess = (res) => {
-=======
 class ContestList extends Component {
   constructor(props) {
     super(props);
@@ -74,7 +21,6 @@ class ContestList extends Component {
     this.setState({ contests });
   };
   handleGoogleLoginSuccess = (res) => {
->>>>>>> Stashed changes
     console.log("Login Success:", res.credential);
     axios.post("http://localhost:3001/user/google-login", {
       token: res.credential,
@@ -88,31 +34,6 @@ class ContestList extends Component {
     this.fetchData();
   }
 
-<<<<<<< Updated upstream
-  return (
-    <>
-      <Nav></Nav>
-      <GoogleLogin
-        onSuccess={handleGoogleLoginSuccess}
-        onError={handleGoogleLoginFailure}
-        useOneTap
-      />
-      <Filter></Filter>
-      <div className={styles.list}>
-        {contests.map((contest) => (
-          <Contest
-            name={contest.name}
-            date={contest.date}
-            address={contest.address}
-            media={contest.media}
-          />
-        ))}
-      </div>
-      <Footer></Footer>
-    </>
-  );
-};
-=======
   render() {
     return (
       <>
@@ -134,6 +55,5 @@ class ContestList extends Component {
     );
   }
 }
->>>>>>> Stashed changes
 
 export default ContestList;
