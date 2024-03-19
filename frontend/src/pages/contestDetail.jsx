c6411a68110f035cfeaed867641d2e148a7817b6
miilearn <126088544+miilearn@users.noreply.github.com>
2024-03-19 20:55:05 +0800

    Update contestDetail.jsx

--- frontend/src/pages/contestDetail.jsx
+++ frontend/src/pages/contestDetail.jsx
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import Nav from "../components/UI/Nav";
+import Footer from "../components/UI/Footer";
+import Banner from "../components/Banner";
+import Ticket from "../components/Ticket";
+import Content from "../components/Content";
+import "./contestDetail.css";
 
-class ContestDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {}
-  render() {
-    return <h1>Hello World</h1>;
-  }
+//create-react-app 
+//class ContestList extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//   }
+//   componentDidMount() {}
+//   render() {
+//     return <h1>Hello World</h1>;
+//   }
+// }
+
+let ContestDetail = () => {
+  return (
+    <div className="contestDetail">
+        <Nav/>
+        <Banner/>
+        <Ticket/>
+        <Content/>
+        <Footer/>
+    </div>
+  );
 }
 
 export default ContestDetail;
