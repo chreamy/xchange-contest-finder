081dd34e0dfc758df876cead73c507207314f618
Karen Lin <karen901208@gmail.com>
2024-04-04 14:42:30 +0800

    Add form components for sign up and login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,24 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { ContestList, ContestDetail } from "./pages";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import Root from "./components/Layout/Root";
 function App() {
   return (
     <GoogleOAuthProvider
       clientId={
         "436307408633-hnr8ld1jgqgmbcohbt068jc4hq34bcvb.apps.googleusercontent.com"
-      }>
+      }
+    >
       <Router>
-        <Routes>
-          <Route path="/" /*element={<Layout />}*/>
-            <Route index element={<ContestList />} />
-            <Route path="contest-list" element={<ContestList />} />
-            <Route path="contest-detail" element={<ContestDetail />} />
-          </Route>
-        </Routes>
+        <Root>
+          <Routes>
+            <Route path="/">
+              <Route index element={<ContestList />} />
+              <Route path="contest-list" element={<ContestList />} />
+              <Route path="contest-detail" element={<ContestDetail />} />
+            </Route>
+          </Routes>
+        </Root>
       </Router>
     </GoogleOAuthProvider>
   );
