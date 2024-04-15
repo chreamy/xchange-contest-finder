c09ef5b8a3deb169c4f32e6fc91da493c22f9df3
Karen Lin <karen901208@gmail.com>
2024-04-15 21:59:13 +0800

    feat: edit route, add root layout and error page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,35 @@
-import "./App.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter as Router,
+  createRoutesFromElements as RoutesElements,
+  RouterProvider,
+} from "react-router-dom";
 import { ContestList, ContestDetail } from "./pages";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import Root from "./components/Layout/Root";
+import ErrorPage from "./pages/ErrorPage";
+
 function App() {
+  // Use React Router v6 syntax
+  const router = Router(
+    RoutesElements(
+      <Route errorElement={<ErrorPage />}>
+        <Route path="/" element={<Root />}>
+          <Route index element={<ContestList />} />
+          <Route path="contest-list" element={<ContestList />} />
+          <Route path="contest-detail" element={<ContestDetail />} />
+        </Route>
+      </Route>
+    )
+  );
+
   return (
     <GoogleOAuthProvider
       clientId={
         "436307408633-hnr8ld1jgqgmbcohbt068jc4hq34bcvb.apps.googleusercontent.com"
       }
     >
-      <Router>
-        <Root>
-          <Routes>
-            <Route path="/">
-              <Route index element={<ContestList />} />
-              <Route path="contest-list" element={<ContestList />} />
-              <Route path="contest-detail" element={<ContestDetail />} />
-            </Route>
-          </Routes>
-        </Root>
-      </Router>
+      <RouterProvider router={router} />
     </GoogleOAuthProvider>
   );
 }
--- frontend/src/components/Layout/Root.jsx
+++ frontend/src/components/Layout/Root.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
 import Footer from "../UI/Footer";
 import Nav from "../UI/Nav";
 import SignUpForm from "../SignUpForm";
 import LoginForm from "../LoginForm";
 
-const Root = ({ children }) => {
+const Root = () => {
   const [signUpIsShown, setSignUpIsShown] = useState(false);
   const [loginIsShown, setLoginIsShown] = useState(false);
 
@@ -30,7 +31,7 @@ const Root = ({ children }) => {
       {signUpIsShown && <SignUpForm onClose={hideSignUpHandler} />}
       {loginIsShown && <LoginForm onClose={hideLoginHandler} />}
       <Nav onShowSignUp={showSignUpHandler} onShowLogin={showLoginHandler} />
-      {children}
+      <Outlet />
       <Footer />
     </>
   );
