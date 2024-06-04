90563e3e92f04511b18687d1f3146241749dd42e
miilearn <a0968340013@gmail.com>
2024-06-04 21:37:12 +0800

    feature: findteam

---
+++ frontend/src/pages/findTeam.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import TeamFilter from "./components/TeamFilter";
+import Team from "./components/Team";
+import styles from "./findTeam.module.css";
+
+  class FindTeam extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        teams: [
+          {
+            "name": "O'Conner Inc",
+            "contest": "66026",
+            "users": "1/5",
+            "description":"jfioihugh",
+          },
+          {
+            "name": "O'Conner Inc",
+            "contest": "66026",
+            "users": "1/5",
+            "description":"jfioihugh",
+          },
+          {
+            "name": "O'Conner Inc",
+            "contest": "66026",
+            "users": "1/5",
+            "description":"jfioihugh",
+          },
+          {
+            "name": "O'Conner Inc",
+            "contest": "66026",
+            "users": "1/5",
+            "description":"jfioihugh",
+          },
+          {
+            "name": "O'Conner Inc",
+            "contest": "66026",
+            "users": "1/5",
+            "description":"jfioihugh",
+          },
+          {
+            "name": "O'Conner Inc",
+            "contest": "66026",
+            "users": "1/5",
+            "description":"jfioihugh",
+          },
+          {
+            "name": "O'Conner Inc",
+            "contest": "66026",
+            "users": "1/5",
+            "description":"jfioihugh",
+          },
+          {
+            "name": "O'Conner Inc",
+            "contest": "66026",
+            "users": "1/5",
+            "description":"jfioihugh",
+          },
+        ],
+      };
+    }
+    // fetchData = async () => {
+    //   const teams = (
+    //     await axios.post("http://localhost:3001/team/", {})
+    //   ).data;
+    //   this.setState({ teams });
+    // };
+
+    // componentDidMount() {
+    //   this.fetchData();
+    // }
+    render() {
+      return (
+        <>
+          <TeamFilter />
+          <div className={styles.list}>
+            {this.state.teams &&
+              this.state.teams.map((team) => (
+                <Team title={team.name}
+                users={team.users}
+                contest={team.contest}
+                description={team.description}
+                link={team.users} />
+              ))}
+          </div>
+        </>
+      );
+    }
+  }
+    
+
+  export default FindTeam;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,3 +2,4 @@ export { default as ContestList } from "./contestList";
 export { default as ContestDetail } from "./contestDetail";
 export { PartnerList } from "./PartnerList";
 export { UserCenter } from "./UserCenter";
+export { default as FindTeam } from "./findTeam";
